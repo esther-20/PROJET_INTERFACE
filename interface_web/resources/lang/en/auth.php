31f5d90c3a00ba453cdb49db71450b19040e951e
225junior <225juniorbrindou@gmail.com>
2020-05-12 09:59:27 +0000

    modiffication ressource/lang/ update from english to french

--- interface_web/resources/lang/en/auth.php
+++ interface_web/resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => "Ces informations d'identification nous sont.",
+    'throttle' => 'Trop de tentatives. Veuillez réessayer dans :seconds secondes.',
 
 ];
